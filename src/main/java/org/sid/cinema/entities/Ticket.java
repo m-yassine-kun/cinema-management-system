f41eeabe5d4220189e3f2aae44039fc6c056c6e5
medYassineHR <harrachmin.m.yassine@gmail.com>
2022-06-09 02:32:01 +0100

    create entities & ORM & interfaces/ first run

---
+++ src/main/java/org/sid/cinema/entities/Ticket.java
@@ -0,0 +1,29 @@
+package org.sid.cinema.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Ticket implements Serializable {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String nomCLient;
+    private double prix;
+    @Column(unique=false, nullable=true)
+    private Integer codePayement;
+    private boolean reserve;
+    @ManyToOne
+    private Place place;
+    @ManyToOne
+    private Projection projection;
+}
